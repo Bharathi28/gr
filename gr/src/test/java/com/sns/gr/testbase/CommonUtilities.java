e01fc3fdfb07bb70437437631e53546afe6fdd70
Bharathi28 <manibharathi@searchnscore.com>
2019-12-23 18:19:42 +0530

    test commit

--- gr/src/test/java/com/sns/gr/testbase/CommonUtilities.java
+++ gr/src/test/java/com/sns/gr/testbase/CommonUtilities.java
@@ -268,7 +268,6 @@ public class CommonUtilities {
 			query = query + include_offer;
 		}
 		query = query + ";";
-//		System.out.println(query);
 			
 		List<Map<String, Object>> locator = DBLibrary.dbAction("fetch",query);
 		return locator;		
